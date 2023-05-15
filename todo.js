9e9de444e2034dbe234bc9fcb095ed9059254ca8
Ian Blake <ian@ianbl8.com>
2023-05-15 13:19:28 +0100

    Display lists of tasks entered

--- todo.js
+++ todo.js
@@ -3,5 +3,11 @@ let count = 0;
 
 function addTodo() {
   count++;
-  console.log(`add todo call ${count}`)
+  const todoText = document.getElementById("todo-id").value;
+  console.log(`add todo call ${count}: ${todoText}`);
+
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todoText;
 }
